103b96102301161d9bae0c474c1d5eea43d5d162
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-09-10 19:13:21 +0530

    git update cread in base

--- tests/base.py
+++ tests/base.py
@@ -363,8 +363,8 @@ class QualtricsBaseTest(BaseCase):
     def get_credentials():
         """Authentication information for the test account."""
         credentials_dict = {}
-        creds = {'clientId': 'TAP_QUALTRICS_CLIENT_ID', 'clientSecret': 'TAP_QUALTRICS_CLIENT_SECRET', 
-                 'scope': 'TAP_QUALTRICS_SCOPE', 'grant_type': 'TAP_QUALTRICS_GRANT_TYPE', 'dataCenter': 'TAP_QUALTRICS_DATA_CENTER'}
+        creds = {'client_id': 'TAP_QUALTRICS_CLIENT_ID', 'client_secret': 'TAP_QUALTRICS_CLIENT_SECRET', 
+                 'scope': 'TAP_QUALTRICS_SCOPE', 'grant_type': 'TAP_QUALTRICS_GRANT_TYPE', 'data_center': 'TAP_QUALTRICS_DATA_CENTER'}
 
         for cred in creds:
             credentials_dict[cred] = os.getenv(creds[cred])
